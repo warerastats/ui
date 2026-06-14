34be6cbbc52f7a9e911f171ced25b693d883aeeb
Aaron Jonk <AJ@jonk.name>
2026-06-14 11:50:37 +0200

    Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,26 @@
+<div class="header">
+    <p>
+        War Era Stats
+    </p>
+</div>
+
+<style lang="scss">
+    div.header {
+        height: 60px;
+        background-color: #0e0e0e;
+        border-bottom: #454545 1px solid;
+
+        display: flex;
+        align-items: center;
+
+        p {
+            text-transform: uppercase;
+            color: #fff;
+            font-family: "Inter", sans-serif;
+            margin: 0;
+            padding-left: 2rem;
+            font-size: 20px;
+            font-weight: 800;
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import favicon from "$lib/assets/favicon.svg";
+    import Header from "$lib/components/Header.svelte";
 
     let { children } = $props();
 </script>
@@ -7,5 +8,7 @@
 <svelte:head>
     <link rel="icon" href={favicon} />
 </svelte:head>
+
+<Header />
 
 {@render children()}
